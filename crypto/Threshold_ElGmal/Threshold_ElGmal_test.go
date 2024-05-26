5a4885126780e6d57c65588d9f375f954f9cfc14
WXY1313 <131689484+WXY1313@users.noreply.github.com>
2024-05-26 11:50:36 +0800

    Update Threshold_ElGmal_test.go

--- crypto/Threshold_ElGmal/Threshold_ElGmal_test.go
+++ crypto/Threshold_ElGmal/Threshold_ElGmal_test.go
@@ -1,22 +1,35 @@
-package Threshold_ElGmal_test
+package Threshold_ElGamal_test
 
 import(
   "dttp/crypto/Threshold_ElGmal"
   "fmt"
   "math/big"
+  "crypto/rand"
   "testing"
+  bn256 "github.com/ethereum/go-ethereum/crypto/bn256/google"
 )
 
+var order=bn256.Order
+
 func TestThresholdElGmalFunction(t *testing.T){
+     //门限值
+     threshold := 3
+     //多少份份额
+     numShares := 7
+	
+     selectedIndices := make([]*big.Int, threshold)
+     for i := 0; i < threshold; i++ {
+	selectedIndices[i] = big.NewInt(int64(i + 1))
+     }
       //定义加密者的公私钥
-        sko,pko:=THEGSetup()
+        sko,pko:=Threshold_ElGamal.THEGSetup()
       //随机生成一个明文信息
 	m,_ := rand.Int(rand.Reader, order)
       //加密明文信息
-	CK:=THEGEncrypt(m,pko)
+	CK:=Threshold_ElGamal.THEGEncrypt(m,pko)
       //生成加密的密钥份额
-	Key:=THEGKenGen(CK, sko, numShares, threshold)
+	Key:=Threshold_ElGamal.THEGKenGen(CK, sko, numShares, threshold)
       //解密密文信息
-	_m:=THEGDecrypt(CK, Key, selectedIndices, threshold)
+	_m:=Threshold_ElGamal.THEGDecrypt(CK, Key, selectedIndices, threshold)
 	fmt.Printf("解密信息为：%s\n",_m.String())
 }
