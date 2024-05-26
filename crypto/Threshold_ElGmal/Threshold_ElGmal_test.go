be1bb606af7265e086817ca6a76685eefe2cd630
WXY1313 <131689484+WXY1313@users.noreply.github.com>
2024-05-26 10:42:57 +0800

    Update Threshold_ElGmal_test.go

--- crypto/Threshold_ElGmal/Threshold_ElGmal_test.go
+++ crypto/Threshold_ElGmal/Threshold_ElGmal_test.go
@@ -9,14 +9,14 @@ import(
 
 func TestThresholdElGmalFunction(t *testing.T){
       //定义加密者的公私钥
-    	sko,pko:=THEGSetup()
+        sko,pko:=THEGSetup()
       //随机生成一个明文信息
-	    m,_ := rand.Int(rand.Reader, order)
+	m,_ := rand.Int(rand.Reader, order)
       //加密明文信息
-	    CK:=THEGEncrypt(m,pko)
+	CK:=THEGEncrypt(m,pko)
       //生成加密的密钥份额
-	    Key:=THEGKenGen(CK, sko, numShares, threshold)
+	Key:=THEGKenGen(CK, sko, numShares, threshold)
       //解密密文信息
-	    _m:=THEGDecrypt(CK, Key, selectedIndices, threshold)
-	    fmt.Printf("解密信息为：%s\n",_m.String())
+	_m:=THEGDecrypt(CK, Key, selectedIndices, threshold)
+	fmt.Printf("解密信息为：%s\n",_m.String())
 }
