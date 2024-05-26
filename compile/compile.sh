4e6d5a6ebbedc3c0df57ab24fe85b1e746b8d953
zhangliang <briliasm@qq.com>
2024-05-26 15:26:36 +0800

    delete useless files

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,3 +1,4 @@
-solc --evm-version paris --optimize --abi ./contract/Verification.sol -o contract --overwrite
-solc --evm-version paris --optimize --bin ./contract/Verification.sol -o contract --overwrite
-abigen --abi=./contract/Verification.abi --bin=./contract/Verification.bin --pkg=contract --out=./contract/Verification.go
+Name=SimpleStorage
+solc --evm-version paris --optimize --abi ./contract/$Name.sol -o contract --overwrite
+solc --evm-version paris --optimize --bin ./contract/$Name.sol -o contract --overwrite
+abigen --abi=./contract/$Name.abi --bin=./contract/$Name.bin --pkg=contract --out=./contract/$Name.go
