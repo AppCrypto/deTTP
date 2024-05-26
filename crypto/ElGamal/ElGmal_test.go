a8beaa578379b9d19714d04e9945c049be641cd1
WXY1313 <131689484+WXY1313@users.noreply.github.com>
2024-05-26 10:29:48 +0800

    Update ElGmal_test.go

--- crypto/ElGamal/ElGmal_test.go
+++ crypto/ElGamal/ElGmal_test.go
@@ -1,1 +1,14 @@
+package ElGmal_test
 
+import(
+      "dttp/crypto/ElGmal"
+      "fmt"
+      "math/big"
+      "testing"
+)
+
+func TestElGmalFunction(t *testing.T){
+      //生成加密者的公私钥对
+      sk,pk:=
+  
+}
