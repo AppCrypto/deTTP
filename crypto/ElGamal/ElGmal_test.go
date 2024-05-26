47f1a8e332b4a08da2ca8286df95ae1bba9c848e
WXY1313 <131689484+WXY1313@users.noreply.github.com>
2024-05-26 10:36:47 +0800

    Update ElGmal_test.go

--- crypto/ElGamal/ElGmal_test.go
+++ crypto/ElGamal/ElGmal_test.go
@@ -9,6 +9,11 @@ import(
 
 func TestElGmalFunction(t *testing.T){
       //生成加密者的公私钥对
-      sk,pk:=ElGmal.
-  
+      sk,pk:=ElGmal.EGSetup()
+      //生成一个明文信息
+      K：= rand.Int(rand.Reader, order)
+      //对该明文信息进行加密
+      EK:=EGEncrypt(K,pk)
+      //对密文信息进行解密
+	_K:=EGDecrypt(EK.EK0,EK.EK1,sk)
 }
