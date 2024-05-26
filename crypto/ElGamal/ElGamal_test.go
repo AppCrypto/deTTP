d5878d745fd8bfb7443ae066f3de1c3008425649
WXY1313 <131689484+WXY1313@users.noreply.github.com>
2024-05-26 11:44:34 +0800

    Update ElGamal_test.go

--- crypto/ElGamal/ElGamal_test.go
+++ crypto/ElGamal/ElGamal_test.go
@@ -3,17 +3,22 @@ package ElGamal_test
 import(
       "dttp/crypto/ElGamal"
       "fmt"
-      "math/big"
       "testing"
+      "crypto/rand"
+      bn256 "github.com/ethereum/go-ethereum/crypto/bn256/google"
 )
+
+var order=bn256.Order
 
 func TestElGamalFunction(t *testing.T){
       //生成加密者的公私钥对
-      sk,pk:=ElGmal.EGSetup()
+      sk,pk:=ElGamal.EGSetup()
       //生成一个明文信息
-      K：= rand.Int(rand.Reader, order)
+      K,_:= rand.Int(rand.Reader, order)
       //对该明文信息进行加密
-      EK:=EGEncrypt(K,pk)
+      EK:=ElGamal.EGEncrypt(K,pk)
       //对密文信息进行解密
-	_K:=EGDecrypt(EK.EK0,EK.EK1,sk)
+	  _K:=ElGamal.EGDecrypt(EK,sk)
+	  fmt.Printf("解密后的明文信息：%s\n",_K)
 }
+
