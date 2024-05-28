15a567d5feb0d4f7471116ba461a10422d6d05c2
WXY1313 <131689484+WXY1313@users.noreply.github.com>
2024-05-28 11:22:07 +0800

    Update ElGamal_test.go

--- crypto/ElGamal/ElGamal_test.go
+++ crypto/ElGamal/ElGamal_test.go
@@ -14,11 +14,12 @@ func TestElGamalFunction(t *testing.T){
       //生成加密者的公私钥对
       sk,pk:=ElGamal.EGSetup()
       //生成一个明文信息
-      K,_:= rand.Int(rand.Reader, order)
+      k,_:=rand.Int(rand.Reader, order)
+      K:=make([]*bn256.G1,1)
+      K[0]= new(bn256.G1).ScalarBaseMult(k)
       //对该明文信息进行加密
-      EK:=ElGamal.EGEncrypt(K,pk)
+      EK:=ElGamal.EGEncrypt(K,pk,len(K))
       //对密文信息进行解密
-	  _K:=ElGamal.EGDecrypt(EK,sk)
+	  _K:=ElGamal.EGDecrypt(EK,sk,len(K))
 	  fmt.Printf("解密后的明文信息：%s\n",_K)
 }
-
