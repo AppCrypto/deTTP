cacb35f43ee718d1483a7bba7c16178443302809
supercool2568 <128695967+supercool2568@users.noreply.github.com>
2024-05-31 23:14:47 +0800

    Update compile.sh
    现在用它编译激励机制和验证合约

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,4 +1,4 @@
-Name=SimpleStorage
+Name=Verification
 solc --evm-version paris --optimize --abi ./contract/$Name.sol -o contract --overwrite
 solc --evm-version paris --optimize --bin ./contract/$Name.sol -o contract --overwrite
 abigen --abi=./contract/$Name.abi --bin=./contract/$Name.bin --pkg=contract --out=./contract/$Name.go
